1422167ad6895052107e79efa7d2d867d3b486e4
CheezItMan <chris@adadevelopersacademy.org>
2019-05-17 06:54:33 -0700

    isPangram outline

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,22 +1,5 @@
 const isPangram = function isPangram(text) {
-  const letterCount = {};
 
-  text.split('').forEach((letter) => {
-    if (letterCount[letter]) {
-      letterCount[letter] += 1;
-    } else {
-      letterCount[letter] = 1;
-    }
-  });
-
-  let isValidPangram = true;
-  'abcdefghijklmnopqrstuvwxyz'.split('').forEach((letter) => {
-    if (letterCount[letter] == undefined) {
-      isValidPangram = false;
-    }
-  });
-
-  return isValidPangram;
 };
 
 module.exports = isPangram;
