2125eab94127bdbf8cfad6c28c71fda333c2f1a1
vwhwang <vwhwang38@gmail.com>
2020-04-01 11:52:35 -0700

    exercise of pangram completed

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,25 @@
 const isPangram = function(text) {
 
+  const alphabets ="abcdefghijklmnopqrstuvwxyz";
+  const alphabets_array = alphabets.split('');
+  const check_alphabet = {};
+
+  alphabets_array.forEach((letter) => {
+    check_alphabet[letter] = false;
+  });
+
+  text.split('').forEach((letter)=>{
+    check_alphabet[letter] = true;
+  });
+
+  for(const letter in check_alphabet){
+    if(!check_alphabet[letter]){
+      return false;
+    }
+  };
+
+  return true;
+
 };
 
 module.exports = isPangram;
--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -5,59 +5,78 @@ describe('isPangram()', () => {
     expect(isPangram).toBeDefined();
   });
 
+
   test('works with a pangram with only lower case', () => {
     // Arrange
     const text = 'the quick brown fox jumps over the lazy dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toBeTruthy();
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
-
+    const text = "abcdefghijklmnopqrstuvwxyz"
     // Act
-
+    const answer = isPangram(text);
     // Assert
+    expect(answer).toBeTruthy();
 
   });
 
   test("missing character 'x'", () => {
     // Arrange
-
+    const text = "abcdefghijklmnopqrstuvwyz"
     // Act
-
+    const answer = isPangram(text);
     // Assert
+    expect(answer).toBeFalsy();
 
   });
 
   test('empty sentence', () => {
     // Arrange
-
+    const text=""
     // Act
-
+    const answer = isPangram(text);
     // Assert
-
+    expect(answer).toBeFalsy();
   });
 
   test('pangram with underscores instead of spaces works', () => {
     // Arrange
+    const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toBeTruthy();
 
   });
 
   test('pangram with numbers', () => {
     // Arrange
+    const text = 'the quick brown fox jumps over the 42 lazy dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toBeTruthy();
 
   });
 
   // Write your own test case
+  test('pangram special cases',() => {
+    const text = '#the quick brown fox jumps over the$$ lazy dog';
+
+    const answer = isPangram(text);
+
+    expect(answer).toBeTruthy();
+  });
+  
 });
